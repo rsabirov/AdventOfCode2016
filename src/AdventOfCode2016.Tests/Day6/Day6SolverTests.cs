fe977639e749119274ded3685abe2e97304bb154
Rail Sabirov <sabirov.rail@gmail.com>
2016-12-13 00:18:40 +0100

    Day6

---
+++ src/AdventOfCode2016.Tests/Day6/Day6SolverTests.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Linq;
+using AdventOfCode2016.Day6;
+using AdventOfCode2016.Tests.TestData;
+using NUnit.Framework;
+
+namespace AdventOfCode2016.Tests.Day6
+{
+    public class Day6SolverTests
+    {
+        [Test]
+        public void Day6Part1InstructionTest()
+        {
+            var path = TestDataHelper.GetPath("Day6InstructionData.txt");
+            var messages = File.ReadLines(path).ToArray();
+
+            var solver = new Day6Solver();
+            var ans = solver.SolvePart1(messages);
+
+            Assert.AreEqual("easter", ans);
+        }
+
+        [Test]
+        public void Day6Part1Test()
+        {
+            var path = TestDataHelper.GetPath("Day6.txt");
+            var messages = File.ReadLines(path).ToArray();
+
+            var solver = new Day6Solver();
+            var ans = solver.SolvePart1(messages);
+
+            Assert.AreEqual("dzqckwsd", ans);
+        }
+
+        [Test]
+        public void Day6Part2InstructionTest()
+        {
+            var path = TestDataHelper.GetPath("Day6InstructionData.txt");
+            var messages = File.ReadLines(path).ToArray();
+
+            var solver = new Day6Solver();
+            var ans = solver.SolvePart2(messages);
+
+            Assert.AreEqual("advent", ans);
+        }
+
+        [Test]
+        public void Day6Part2Test()
+        {
+            var path = TestDataHelper.GetPath("Day6.txt");
+            var messages = File.ReadLines(path).ToArray();
+
+            var solver = new Day6Solver();
+            var ans = solver.SolvePart2(messages);
+
+            Assert.AreEqual("lragovly", ans);
+        }
+    }
+}
---
+++ src/AdventOfCode2016/Day6/Day6Solver.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace AdventOfCode2016.Day6
+{
+    public class Day6Solver
+    {
+        public string SolvePart1(string[] messages)
+        {
+            Debug.Assert(messages.All(m => m.Length == messages.First().Length));
+
+            var mostFrequentCharsForEachPosition = Enumerable
+                .Range(0, messages.First().Length)
+                .Select(i => messages
+                    .Select(message => message[i])
+                    .GroupBy(c => c, (c, items) => new {Char = c, Count = items.Count()})
+                    .OrderByDescending(group => group.Count)
+                    .Select(group => group.Char)
+                    .First()
+                )
+                .ToArray();
+
+            return new string(mostFrequentCharsForEachPosition);
+        }
+
+        public string SolvePart2(string[] messages)
+        {
+            Debug.Assert(messages.All(m => m.Length == messages.First().Length));
+
+            var mostFrequentCharsForEachPosition = Enumerable
+                .Range(0, messages.First().Length)
+                .Select(i => messages
+                    .Select(message => message[i])
+                    .GroupBy(c => c, (c, items) => new { Char = c, Count = items.Count() })
+                    .OrderBy(group => group.Count)
+                    .Select(group => group.Char)
+                    .First()
+                )
+                .ToArray();
+
+            return new string(mostFrequentCharsForEachPosition);
+        }
+    }
+}
