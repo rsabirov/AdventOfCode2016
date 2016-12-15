f01fd684dab6fd1081bb051041f857d21dafea2d
Rail Sabirov <sabirov.rail@gmail.com>
2016-12-15 16:41:14 +0100

    Day8

---
+++ src/AdventOfCode2016.Tests/Day8/Day8SolverTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AdventOfCode2016.Day8;
+using AdventOfCode2016.Tests.TestData;
+using NUnit.Framework;
+
+namespace AdventOfCode2016.Tests.Day8
+{
+    public sealed class Day8SolverTests
+    {
+        [Test]
+        public void Day8Part1InstructionTest()
+        {
+            var instructions = new[]
+            {
+                "rect 3x2",
+                "rotate column x=1 by 1",
+                "rotate row y=0 by 4",
+                "rotate column x=1 by 1"
+            };
+
+            var solver = new Day8Solver();
+            var ans = solver.Day8SolvePart1(instructions, w: 7, h: 3);
+
+            Assert.AreEqual(6, ans);
+        }
+
+        [Test]
+        public void Day8Part1Test()
+        {
+            var path = TestDataHelper.GetPath("Day8.txt");
+            var instructions = File.ReadLines(path).ToArray();
+
+            var solver = new Day8Solver();
+            var ans = solver.Day8SolvePart1(instructions);
+
+            Assert.AreEqual(123, ans);
+        }
+
+        [Test]
+        public void Day8Part2Test()
+        {
+            var path = TestDataHelper.GetPath("Day8.txt");
+            var instructions = File.ReadLines(path).ToArray();
+
+            var solver = new Day8Solver();
+            var matrix = solver.Day8SolvePart2(instructions);
+
+            // writing resulting matrix to file
+            var filePath = Path.GetTempFileName();
+            WriteMatrixToFile(matrix, filePath);
+
+            //HINT: for getting result checkout the file
+            Console.WriteLine("Result saved in '{0}'", filePath);
+        }
+
+        private static void WriteMatrixToFile(bool[,] matrix, string fileName)
+        {
+            using (var file = File.OpenWrite(fileName))
+            using (var writter = new StreamWriter(file))
+            {
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(0); j++)
+                        writter.Write("{0} ", matrix[j, i] ? '#' : '.');
+                    writter.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ src/AdventOfCode2016/Day8/Day8Solver.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2016.Day8
+{
+    public sealed class Day8Solver
+    {
+        public int Day8SolvePart1(string[] instructions, int w = 50, int h = 6)
+        {
+            var matrix = ExecuteInstructions(instructions, h, w);
+
+            var result = Enumerable.Range(0, w)
+                .Select(i => Enumerable.Range(0, h).Select(j => matrix[i, j] ? 1 : 0).Sum())
+                .Sum();
+
+            return result;
+        }
+
+        public bool[,] Day8SolvePart2(string[] instructions, int w = 50, int h = 6)
+        {
+            return ExecuteInstructions(instructions, h, w);
+        }
+
+        private static bool[,] ExecuteInstructions(string[] instructions, int h, int w)
+        {
+            var a = new bool[w, h];
+
+            foreach (var instruction in instructions)
+            {
+                var parts = instruction.Split(' ');
+                var command = parts[0];
+
+                switch (command)
+                {
+                    case "rect":
+                        var paramParts = parts[1].Split(new[] {"x"}, StringSplitOptions.RemoveEmptyEntries);
+                        var rw = int.Parse(paramParts[0]);
+                        var rh = int.Parse(paramParts[1]);
+
+                        for (int i = 0; i < Math.Min(rw, w); i++)
+                        for (int j = 0; j < Math.Min(rh, h); j++)
+                            a[i, j] = true;
+
+                        break;
+
+                    case "rotate":
+                        if (parts[1] == "row")
+                        {
+                            var ry = int.Parse(parts[2].Substring(2)); // removing "y="
+                            var rby = int.Parse(parts[4]);
+                            if (ry >= h)
+                                continue;
+
+                            var trow = new bool[w];
+                            for (int x = 0; x < w; x++)
+                                trow[(x + rby) % w] = a[x, ry];
+                            for (int x = 0; x < w; x++)
+                                a[x, ry] = trow[x];
+                        }
+                        else
+                        {
+                            var cx = int.Parse(parts[2].Substring(2)); // removing "x="
+                            var cby = int.Parse(parts[4]);
+
+                            if (cx >= w)
+                                continue;
+                            var tcol = new bool[h];
+                            for (int y = 0; y < h; y++)
+                                tcol[(y + cby) % h] = a[cx, y];
+                            for (int y = 0; y < h; y++)
+                                a[cx, y] = tcol[y];
+                        }
+                        break;
+
+                    default:
+                        throw new NotSupportedException();
+                }
+            }
+            return a;
+        }
+    }
+}
